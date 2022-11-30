3ba85593a7842ceeded4320e5b1a86ca72f86d8d
roxanapirlea <roxanap.dev@gmail.com>
2022-11-30 20:30:23 +0100

    Add more file reading methods

--- src/Utils.kt
+++ src/Utils.kt
@@ -5,8 +5,20 @@ import java.security.MessageDigest
 /**
  * Reads lines from the given input txt file.
  */
-fun readInput(name: String) = File("src", "$name.txt")
+fun readInput(name: String) = File("src/inputs", "$name.txt")
     .readLines()
+
+/**
+ * Reads text from the given input txt file.
+ */
+fun readText(fileName: String) =
+    File("src/inputs", "$fileName.txt").readText()
+
+/**
+ * Reads text from the given input txt file and splits it into a list.
+ */
+fun readSplitText(fileName: String, separator: String) =
+    File("src/inputs", "$fileName.txt").readText().trim().split(separator)
 
 /**
  * Converts string to md5 hash.
