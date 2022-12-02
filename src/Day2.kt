734e9cf2896f7fc8766f340a08549bc93b002e05
roxanapirlea <roxanap.dev@gmail.com>
2022-12-02 08:03:19 +0100

    Solve day 2 part 2

--- src/Day2.kt
+++ src/Day2.kt
@@ -16,6 +16,16 @@ fun main() {
             else -> throw IllegalArgumentException()
         }
 
+    fun String.outcomeToRPSScore(opponent: Int): Int {
+        val score = when (this) {
+            "X" -> (opponent-1).mod(3)
+            "Y" -> opponent
+            "Z" -> (opponent+1).mod(3)
+            else -> throw IllegalArgumentException()
+        }
+        return if (score == 0) 3 else score
+    }
+
     fun part1(input: List<String>): Long {
         return input.map {
             val opponent = it.substringBefore(" ").toRPSScore()
@@ -25,10 +35,21 @@ fun main() {
         }.reduce { total, score -> total + score }
     }
 
+    fun part2(input: List<String>): Long {
+        return input.map {
+            val opponent = it.substringBefore(" ").toRPSScore()
+            val me = it.substringAfter(" ").outcomeToRPSScore(opponent)
+            val outcome = getOutcomeScore(me, opponent)
+            outcome + me.toLong()
+        }.reduce { total, score -> total + score }
+    }
+
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day02_test")
     check(part1(testInput) == 15L)
+    check(part2(testInput) == 12L)
 
     val input = readInput("Day02")
     println(part1(input))
+    println(part2(input))
 }
