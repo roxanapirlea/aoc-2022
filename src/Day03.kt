b2c3c56d3c68d1a60c1b11154befdb5a1776489f
roxanapirlea <roxanap.dev@gmail.com>
2022-12-03 10:17:10 +0100

    Solve part 2

--- src/Day03.kt
+++ src/Day03.kt
@@ -16,10 +16,21 @@ fun main() {
         }.reduce { total, priority -> total + priority }
     }
 
+    fun part2(input: List<String>): Long {
+        return input.chunked(3)
+            .map { group -> group.map { backpack -> backpack.toCharArray().toSet() } }
+            .map { group ->
+                val common = group.reduce { common, backpack -> common intersect backpack }.single()
+                common.getPriority()
+            }.reduce { total, priority -> total + priority }
+    }
+
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day03_test")
     check(part1(testInput) == 157L)
+    check(part2(testInput) == 70L)
 
     val input = readInput("Day03")
     println(part1(input))
+    println(part2(input))
 }
