6daace2cd8f3680088a0fc3ac6ce4e3438440484
roxanapirlea <roxanap.dev@gmail.com>
2022-12-06 08:02:08 +0100

    Solve Day 6 part 2

--- src/Day06.kt
+++ src/Day06.kt
@@ -13,6 +13,10 @@ fun main() {
         return getStartMarker(input, 4)
     }
 
+    fun part2(input: String): Int {getStartMarker(input, 14)
+        return getStartMarker(input, 14)
+    }
+
     // test if implementation meets criteria from the description
     check(part1("mjqjpqmgbljsphdztnvjfqwrcgsmlb") == 7)
     check(part1("bvwbjplbgvbhsrlpgdmjqwftvncz") == 5)
@@ -20,6 +24,13 @@ fun main() {
     check(part1("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg") == 10)
     check(part1("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw") == 11)
 
+    check(part2("mjqjpqmgbljsphdztnvjfqwrcgsmlb") == 19)
+    check(part2("bvwbjplbgvbhsrlpgdmjqwftvncz") == 23)
+    check(part2("nppdvjthqldpwncqszvftbrmjlhg") == 23)
+    check(part2("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg") == 29)
+    check(part2("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw") == 26)
+
     val input = readText("Day06")
     println(part1(input))
+    println(part2(input))
 }
