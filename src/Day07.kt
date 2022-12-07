ec96795484005e8dfc307922969daca77070e8f7
roxanapirlea <roxanap.dev@gmail.com>
2022-12-07 09:06:50 +0100

    Solve Day 7 part 2

--- src/Day07.kt
+++ src/Day07.kt
@@ -81,16 +81,36 @@ fun main() {
         return files
     }
 
+    fun getDirectoryToDeleteSize(file: File, minSize: Long, prevMaxSize: Long): Long {
+        var maxSize = prevMaxSize
+        if (file.fileSize in minSize until maxSize && file.children.isNotEmpty()) {
+            maxSize = file.fileSize
+        }
+        file.children.forEach { child ->
+            val size = getDirectoryToDeleteSize(child, minSize, maxSize)
+            if (size < maxSize) maxSize = size
+        }
+        return maxSize
+    }
+
     fun part1(input: List<String>): Long {
         val root = buildFileSystem(input)
         return getSmallDirectories(root, 100000, mutableSetOf())
             .sumOf { it.fileSize }
     }
 
+    fun part2(input: List<String>): Long {
+        val root = buildFileSystem(input)
+        val minDeleteSize = root.fileSize - 40000000
+        return getDirectoryToDeleteSize(root, minDeleteSize, root.fileSize)
+    }
+
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day07_test")
     check(part1(testInput) == 95437L)
+    check(part2(testInput) == 24933642L)
 
     val input = readInput("Day07")
     println(part1(input))
+    println(part2(input))
 }
