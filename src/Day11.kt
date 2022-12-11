6c4ae6a70678ca361404edabd1e7d1ed11accf32
roxanapirlea <roxanap.dev@gmail.com>
2022-12-11 12:36:40 +0100

    Solve Day 11 part 2

--- src/Day11.kt
+++ src/Day11.kt
@@ -49,7 +49,7 @@ fun main() {
         return monkeys
     }
 
-    fun List<Monkey>.visit(repetitions: Int, worryDivider: Int): Long {
+    fun List<Monkey>.visit(): Long {
         val visits = MutableList(size) { 0 }
         repeat(20) {
             for (i in indices) {
@@ -74,14 +74,60 @@ fun main() {
         return max1.toLong() * max2.toLong()
     }
 
+    fun List<Monkey>.visitPart2(): Long {
+        val totalDivider = this.map { it.testDivider }.reduce { acc, d -> acc * d }
+        val visits = MutableList(size) { 0 }
+        repeat(10000) {
+            for (i in indices) {
+                val items = this[i].items
+                val newItems = items.map { item ->
+                    var remainder = item
+                    when (this[i].operation.first) {
+                        Operation.MULTIPLY -> {
+                            remainder *= this[i].operation.second
+                            if (remainder > totalDivider) {
+                                remainder %= totalDivider
+                            }
+                        }
+
+                        Operation.ADD -> {
+                            remainder += this[i].operation.second
+                            if (remainder > totalDivider) {
+                                remainder -= totalDivider
+                            }
+                        }
+
+                        Operation.SQUARE -> {
+                            remainder *= remainder
+                        }
+                    }
+                    val testTrue = remainder % this[i].testDivider == 0L
+                    remainder to testTrue
+                }
+                this[this[i].monkeyTrue].items.addAll(newItems.filter { it.second }.map { it.first })
+                this[this[i].monkeyFalse].items.addAll(newItems.filter { !it.second }.map { it.first })
+                visits[i] = visits[i] + this[i].items.size
+                this[i].items.clear()
+            }
+        }
+        val (max1, max2) = visits.sortedDescending().take(2)
+        return max1.toLong() * max2.toLong()
+    }
+
     fun part1(input: List<String>): Long {
-        return input.monkeyList().visit(20, 3)
+        return input.monkeyList().visit()
+    }
+
+    fun part2(input: List<String>): Long {
+        return input.monkeyList().visitPart2()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day11_test")
     check(part1(testInput) == 10605L)
+    check(part2(testInput) == 2713310158L)
 
     val input = readInput("Day11")
     println(part1(input))
+    println(part2(input))
 }
